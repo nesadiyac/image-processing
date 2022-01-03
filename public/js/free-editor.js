bcf6986830d15fb130354b8adee7c8e9c5534edc
nesadiyac <nesadiyac@gmail.com>
2022-01-02 21:54:53 -0500

    image editor

--- public/js/free-editor.js
+++ public/js/free-editor.js
@@ -1,17 +1,1 @@
-let file;
-const brightness = document.querySelector('#brightness');
-document.querySelector('#upload').addEventListener('change', event => {
-    file = event.target.files[0];
-    const urlCreator = window.URL || window.webkitURL;
-    document.getElementById("input-image").src = urlCreator.createObjectURL(file);
-    document.getElementById("input-image").style.height = '50vh';
-    Caman("#input-image", function () {
-        this.brightness(5).render();
-    });
-})
-brightness.addEventListener('click', e => {
-    Caman("#input-image", function () {
-        this.brightness(500).render();
-    });
-    e.preventDefault();
-})
+
